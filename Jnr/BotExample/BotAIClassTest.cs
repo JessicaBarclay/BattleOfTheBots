41cc6a04f85e56eb0623b283f043c6e5d3114892
Barclay <BARCLAJX@legal.regn.net>
2017-07-28 21:58:17 +0100

    Bot beats MirrorBot using pattern recognition

---
+++ Jnr/BotExample/BotAIClassTest.cs
@@ -0,0 +1,17 @@
+﻿namespace BotExample
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BotTest
+    {
+        [Test]
+        public void NumberOfDynamite()
+        {
+            BotAIClass TestBot = new BotAIClass();
+            TestBot.SetStartValues("TestOponent", 50, 100, 10);
+            
+            Assert.AreEqual(10, TestBot._dynamite);
+        }
+    }
+}
---
+++ Jnr/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BotExample;
+
+
+namespace BotExample.Tests
+{
+    [TestClass]
+    public class BotTest
+    {
+        [TestMethod]
+        public void NumberOfDynamite()
+        {
+            BotAIClass.SetStartValues("TestOponent", 50, 100, 10);
+
+            Assert.AreEqual(10, BotAIClass._dynamite);
+        }
+    }
+}
